1e9acf9d69d19f85e43683d2f34b3d27e8e880ce
Anurag-12 <64366327+Anurag-12@users.noreply.github.com>
2020-05-09 01:58:08 +0530

    Add files via upload

---
+++ src/test/java/com/anurag/openshift/project/SpringOpenshiftApplicationTests.java
@@ -0,0 +1,14 @@
+package com.anurag.openshift.project;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
